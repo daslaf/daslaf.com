2c8b978ebe51befb3a7b882ba4ee590a740a4d49
Osman Cea <osman.cea@gmail.com>
2020-12-07 23:39:32 -0300

    feat: add list of aoc posts to data

---
+++ src/_data/aoc.js
@@ -0,0 +1,30 @@
+module.exports = [
+  {
+    url: "../aoc-1-report-pair",
+    title: "Día 1 - Report Repair"
+  },
+  {
+    url: "../aoc-2-password-philosophy",
+    title: "Día 2 - Password Philosophy"
+  },
+  {
+    url: "../aoc-3-toboggan-trajectory",
+    title: "Día 3 - Toboggan Trajectory"
+  },
+  {
+    url: "../aoc-4-passport-processing",
+    title: "Día 4 - Passport Processing"
+  },
+  {
+    url: "../aoc-5-binary-boarding",
+    title: "Día 5 - Binary Boarding"
+  },
+  {
+    url: "../aoc-6-custom-customs",
+    title: "Día 6 - Custom Customs"
+  },
+  {
+    url: "../aoc-7-handy-haversacks",
+    title: "Día 7 - Handy Haversacks"
+  },
+];
